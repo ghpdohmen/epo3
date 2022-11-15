453748275fb416d709ffa6094ecbf3bf38579fa0
John Doe <johndoe@example.com>
2022-11-15 11:13:15 +0100

    trest

--- vhdl/timebase.vhd
+++ vhdl/timebase.vhd
@@ -3,7 +3,7 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 --copied from digital systems b
---25 MHz clock coming in, 15 kHz coming out
+--25 MHz clock coming in, 15 kHz coming out\
 entity timebase is
 	generic (
         CLK_SCALE : INTEGER
