0da6951812cebe51aae3a1236b7662034bbd4381
Guus Dohmen <guusdohmen@hotmail.nl>
2022-11-22 09:39:15 +0100

    fix voor timebase?!

--- vhdl/timebase.vhd
+++ vhdl/timebase.vhd
@@ -3,33 +3,48 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity timebase is
-	port (	
-			clk		: in	std_logic;
-			reset 	: in	std_logic;
-			clk15k 	: out 	std_logic;
+	port (	clk		: in	std_logic;
+		reset		: in	std_logic;
+		clk15k		: out	std_logic;
 	);
 end entity timebase;
 
-architecture behav of timebase is
-	signal count, new_count : unsigned (9 downto 0);
-	begin 
-		process(clk)
-		begin
-			if(reset = '1') then
-				count <= '0000000000';
+
+architecture behavioural of timebase is
+
+	signal count , new_count : unsigned (9 downto 0) ;
+	signal currentPulse 	 : std_logic;
+
+begin
+	--This process generates the register
+	process(clk)
+	begin
+		if (clk'event and clk = '1') then
+			if (reset = '1') then
+				count <= (others => '0'); --set a 0 to reset
 			else
-				if(rising_edge(clk)) then
-					if(to_integer(count) > 833) then
-						count <= 0;
-						if(clk15k = '0') then
-							clk15k <= '1';
-						else
-							clk15k <= '0';
-						end if;
-					else
-					count <= to_unsigned(to_integer(count) + 1);
-					end if;
-				end if;
+				count <= new_count;
 			end if;
-		end process;
-end architecture behav;
+		end if;
+	end process;
+
+
+	--This process calculates the new count-value
+	process(count)
+	begin
+		if(count >= std_logic_vector(to_unsigned(833,9)))
+			if(currentPulse = 1)
+				currentPulse <= 0;
+			else
+				currentPulse <= 1;
+			end if;
+			new_count <= std_logic_vector(to_unsigned(0,9));
+		else
+			new_count <= count + 1;
+		end if;
+	end process;
+
+	--With this line, the pulse value is brought to the outside
+	clk15k <= currentPulse;
+
+end architecture behavioural;
