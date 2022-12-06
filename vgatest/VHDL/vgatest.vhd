3bc695dcbe1c4b045394fcfaee04f3e642aad7bd
YashMD12 <104067503+YashMD12@users.noreply.github.com>
2022-12-06 10:28:40 +0100

    Update vgatest.vhd

--- vgatest/VHDL/vgatest.vhd
+++ vgatest/VHDL/vgatest.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 entity vgatest is
-port(clock         : in std_logic;
-       R, G, B, H, V : out std_logic);
+port(clock, enable         : in std_logic;
+       R, G, B, H, V, displayenable : out std_logic);
 end vgatest;
 
