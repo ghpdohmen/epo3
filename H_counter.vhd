1b1d0df7fc7c0d0e6d33102f0bb5b0c3b1e8686d
NouraDK <68608054+PhObOs20@users.noreply.github.com>
2023-01-13 16:45:19 +0100

    Update H_counter.vhd

--- H_counter.vhd
+++ H_counter.vhd
@@ -10,32 +10,41 @@ logic_h_out: out std_logic_vector (3 downto 0)
 );
 end;
 architecture behav of h_counter is
-    signal count, new_count: unsigned (3 downto 0);
+component edge_detector is 
+port(
+	clk     : in  std_logic;
+        input  : in  std_logic;
+        edges : out std_logic
+);
+end component;
+    signal sig_edges: std_logic;
+    signal count_h, new_count_h: unsigned (3 downto 0);
 begin
+l_edge: edge_detector port map (clk => clk, input => logic_h_32, edges=>sig_edges);
     process(clk,reset)
         begin
             if (rising_edge(clk)) then
                 if (reset = '1') then
-                   count <= (others => '0');
+                   count_h <= (others => '0');
 		    --new_count <= (others => '0');
                 else
-                    count <= new_count;
+                    count_h <= new_count_h;
                 end if;
             end if;
     end process;
  
-    process(logic_h_32, count)
+    process(sig_edges, count_h)
     begin
-    if (logic_h_32 ='1') then
-	    if (count="1110") then
-        	new_count <= (others => '0');
+    if (sig_edges ='1') then
+	    if (count_h="1110") then
+        	new_count_h <= (others => '0');
 	    else
-        	new_count <= count + 1;
+        	new_count_h <= count_h + 1;
 	    end if;
 	else 
-	new_count <= count;
+	new_count_h <= count_h;
 	end if;
     end process;
-    logic_h_out <= std_logic_vector(count);
+logic_h_out <= std_logic_vector(count_h);
 end architecture behav;
 
