dbf142843f87284c509ebc3d667df5f463d62d27
NouraDK <68608054+PhObOs20@users.noreply.github.com>
2023-01-13 16:47:53 +0100

    Update graph_logic.vhd

--- graph_logic.vhd
+++ graph_logic.vhd
@@ -65,7 +65,7 @@ component e_counter is
 	logic_e_out: out std_logic_vector (9 downto 0)
    );
 end component;
-component countdown_bar is      -- ga eerst zonder proberen
+component countdown_bar is      
    port(
   	 mouse_count: in std_logic;
 	 countdown_aan: in std_logic;
@@ -87,19 +87,20 @@ lh: h_counter port map (logic_h_32 => logic_h_32_minis, logic_h_out => local_x, 
 le: e_counter port map (logic_v_out => local_y, logic_h_out => local_x, 
 --logic_h => minis_enable, 
 muis_x => logic_x, muis_y => logic_y, logic_e_out => logic_e_asked, clk => clk, reset => reset);
+lcountdown: countdown_bar port map (mouse_count => mouse_count, countdown_aan => countdown_aan, middelste_knop => middelste_knop, 
+countdown_out => sig_countdown, 
+countdown_klaar => countdown_klaar, clk => clk, reset => reset ); 
 logic_y_asked <= local_y;
 logic_x_asked <= local_x;
 countdown_int <= 14-(to_integer(unsigned(sig_countdown)) / 2);
-lcountdown: countdown_bar port map (mouse_count => mouse_count, countdown_aan => countdown_aan, middelste_knop => middelste_knop, 
-countdown_out => sig_countdown, 
-countdown_klaar => countdown_klaar,clk => clk, reset => reset );  --  ga eerst zonder proberen
-process(clk)
+
+process(local_y, local_x, logic_y, logic_x, logic_rom_colour, countdown_int, logic_rom_colour)
     begin
-    if (local_y=logic_y and local_x=logic_x) then --zit de muis er overheen?              
-        if (logic_rom_colour = "01" ) then
-            if (("0000"<local_x) and (local_x<"1011") and ("0100"<local_y)) then
+    if (local_y=logic_y and local_x=logic_x) then -- is the cursor on the cell              
+        if (logic_rom_colour = "01" ) then -- behind the cursor
+            if (("0000"<local_x) and (local_x<"1011") and ("0100"<local_y)) then --canvas
                 colour_output <= logic_ram_colour;
-            elsif((local_y = "0011") and (local_x /= "1110")) then  --- ga eerst zonder proberen
+            elsif((local_y = "0011") and (local_x /= "1110")) then --countdown
                 if (countdown_int > to_integer(unsigned(local_x))) then
                     colour_output <= "000";
                 else
@@ -108,23 +109,23 @@ process(clk)
             else
                 colour_output <= "000";
             end if;
-        elsif (logic_rom_colour = "00" ) then
+        elsif (logic_rom_colour = "00" ) then -- contours of the cursor
             colour_output <= "000";
-        elsif (logic_rom_colour = "10" ) then
-            colour_output <= loaded_colour;
+        elsif (logic_rom_colour = "10" ) then -- main colour of the cursor
+            colour_output <= loaded_colour; 
         else
            colour_output <= "111";
         end if;
-    elsif (("0000"<local_x) and (local_x<"1011") and ("0100"<local_y)) then--zijn de coordinaten in het tekenvlak
+   elsif (("0000"<local_x) and (local_x<"1011") and ("0100"<local_y)) then--canvas
         colour_output <= logic_ram_colour;
-   elsif((local_y="0011") and (local_x/="1110")) then --- ga eerst zonder proberen
+   elsif((local_y="0011") and (local_x/="1110")) then --countdown
         if (countdown_int > to_integer(unsigned(local_x))) then
                     colour_output <= "000";
                 else
                     colour_output <= "010";
         end if;          
     else 
-    colour_output <= "000";
+    colour_output <= "000"; -- background
     end if;
 
 end process;
