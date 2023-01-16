6bd9d38f00d0cd18298b9e8be48de99ba5fe88a2
NouraDK <68608054+PhObOs20@users.noreply.github.com>
2023-01-16 14:01:56 +0100

    Update countdown.vhd

--- countdown.vhd
+++ countdown.vhd
@@ -3,74 +3,72 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 entity countdown_bar is 
 port(
-	mouse_count: in std_logic;
+	enable: in std_logic;
 	countdown_aan: in std_logic;
     	middelste_knop: in std_logic;
 	clk: in std_logic;
 	reset: in std_logic;
 	countdown_klaar: out std_logic;
-	countdown_out: out std_logic_vector(4 downto 0)
+	countdown_out: out std_logic_vector(18 downto 0)
 );
 end;
-architecture behav of countdown_bar is
-    signal count, new_count: unsigned (6 downto 0);
-    signal count_sec: std_logic_vector (6 downto 0); -- telt 100 mouse pulsen voor 1 seconde
-    signal pulse_28: std_logic; -- telt 30 seconden
-    signal count_28, new_count_28: unsigned (4 downto 0);
+architecture behav of countdown_bar is -- +-30 seconden lang het enable signaal op de falling edge tellen, 20 bits
+component edge_det_fall is 
+port(
+	clk     : in  std_logic;
+        input  : in  std_logic;
+        edges : out std_logic
+);
+end component;
+signal sig_edge_fall: std_logic;
+signal count_c, new_count_c, new_count2_c: unsigned (18 downto 0);
 begin
-    process(clk)
-        begin
-            if (rising_edge(clk)) then
-                if ((reset = '1') or (middelste_knop='1')) then
-                    count <= (others => '0');
-                else
-                    count <= new_count;
-                end if;
-            end if;
-    end process;
- 
-    process(mouse_count)
-    begin
-	if (mouse_count ='1' and countdown_aan='1') then
-        if (count_sec ="1100011") then -- 99
-            new_count <= "0000000";
-            pulse_28 <= '1';
-        else
-            new_count <= count + 1;
-            pulse_28<= '0';
-        end if;
-	else 
-	new_count <= new_count;
-	end if;
-    end process;
-count_sec <= std_logic_vector(count);
+l_edge: edge_det_fall port map (clk => clk, input => enable, edges=>sig_edge_fall);
 
-process(clk) -- twee keer dezelfde process
+process(clk, reset, new_count_c, new_count2_c) -- global reset
         begin
             if (rising_edge(clk)) then
-                if ((reset = '1') or (middelste_knop='1')) then
-                    count <= (others => '0');
+                if (reset = '1') then
+                    count_c <= (others => '0');  -- in if else dus goed
                 else
-                    count <= new_count;
+                    if (new_count2_c= "0000000000000000000") then
+                    	count_c <= new_count2_c; -- goed
+                    else
+                    	count_c <= new_count_c; -- goed		
+		    end if;	
                 end if;
             end if;
-    end process;
- 
-    process(mouse_count)
-    begin
-	if (pulse_28 ='1' and countdown_aan='1') then
-            new_count_28 <= count_28 + 1;
-	else 
-	new_count <= new_count;
-	end if;
-    end process;
-countdown_out <= std_logic_vector(count_28);
-process(count_28)
-begin
-if (count_28="11100") then -- +-28 seconden
-	countdown_klaar <= '1';
-else
-	countdown_klaar <= '0';
-end if;
 end process;
+
+process(clk, middelste_knop, new_count_c) -- local reset
+        begin
+            if (rising_edge(clk)) then
+                if (middelste_knop = '1') then
+		    new_count2_c <= (others => '0');	 -- goed
+                else
+                    new_count2_c <= new_count_c;  -- goed
+                end if;
+            end if;
+end process;
+
+process(sig_edge_fall, count_c)  -- counting process, automatic overflow
+    begin
+	if (sig_edge_fall ='1') then
+        	new_count_c <= count_c + 1;
+	else 
+		new_count_c <= count_c;
+	end if;
+end process;
+
+process(count_c) -- when the countdown is at 30? seconds countdown_klaar= 1
+	begin
+		if (count_c = "1111111111111111111") then
+			countdown_klaar <= '1';
+		else
+			countdown_klaar <= '0';
+		end if;
+end process;
+
+countdown_out <= std_logic_vector(count_c);
+
 end architecture behav;
