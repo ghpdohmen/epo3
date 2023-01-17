9edf4509e14acbbff71d53b70772dfcd352e4478
edindegeweldige <edintuinstra@gmail.com>
2023-01-17 11:48:18 +0100

    Delay toegevoegd in main_fsm zodat counter in data stream niet te veel count

--- gwtf/VHDL/main_fsm.vhd
+++ gwtf/VHDL/main_fsm.vhd
@@ -6,9 +6,12 @@ entity main_fsm is
 	reset:		in  std_logic;
         data_in:        in std_logic_vector(10 downto 0);
         count15k_in:    in std_logic_vector(3 downto 0);
+	count25M:		in std_logic_vector(12 downto 0);
 	handshake_in:   in std_logic;
         bit11_reg_rst:  out std_logic;
         cntReset15k:    out std_logic;
+	cntReset25M:		out std_logic;
+	mux_select_main: out std_logic;
         actBit:         out std_logic;
         send_reset:     out std_logic;
 	x_out:          out std_logic_vector(2 downto 0);
