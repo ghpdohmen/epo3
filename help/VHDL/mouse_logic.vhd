118eb0c01cdaeec9a0dc86d125238bf138b7b536
EdindeGeweldige <edintuinstra@gmail.com>
2023-01-19 16:56:26 +0100

    Routed state leds to the output of pixel

--- help/VHDL/mouse_logic.vhd
+++ help/VHDL/mouse_logic.vhd
@@ -15,7 +15,17 @@ entity mouse_logic is
 	tempx: out std_logic_vector(3 downto 0);
 	tempy: out std_logic_vector(3 downto 0);
 	draw: out std_logic;
-	middelsteknop: out std_logic);
+	middelsteknop: out std_logic;
+		  rst				 : out std_logic;
+	led0		: out std_logic;
+	led1		: out std_logic;
+	led2		: out std_logic;
+	led3		: out std_logic;
+	led5		: out std_logic;
+	led6		: out std_logic;
+	led7		: out std_logic;
+	led8		: out std_logic;
+	led9		: out std_logic);
 
 end mouse_logic;
 
