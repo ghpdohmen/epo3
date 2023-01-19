8380a6fa5f038d10927b70c4a6b44afb0dc5f4e1
naufalelkhatibi <106252099+naufalelkhatibi@users.noreply.github.com>
2023-01-19 17:03:08 +0100

    Update logic_top-behaviour_logic_top.vhd

--- help/VHDL/logic_top-behaviour_logic_top.vhd
+++ help/VHDL/logic_top-behaviour_logic_top.vhd
@@ -1,14 +1,14 @@
 library IEEE;
 use IEEE.std_logic_1164.ALL;
 
-architecture behaviour_logic_top of logic_top is
-	
+architecture behaviour_top of top is
+
 component x is
    port(clk         : in  std_logic;
         reset       : in  std_logic;
         dx          : in  std_logic_vector(3 downto 0);
         handshakemi : in  std_logic;
-        tempx       : buffer std_logic_vector(3 downto 0);
+        tempx       : out std_logic_vector(3 downto 0);
         handshakeimx : out std_logic);
 end component;
 
@@ -17,7 +17,7 @@ component y is
         reset       : in  std_logic;
         dy          : in  std_logic_vector(3 downto 0);
         handshakemi : in  std_logic;
-        tempy       : buffer std_logic_vector(3 downto 0);
+        tempy       : out std_logic_vector(3 downto 0);
         handshakeimy : out std_logic);
 end component;
 
@@ -30,18 +30,21 @@ component color is
 	output_color: out std_logic_vector(2 downto 0);
 	draw: out std_logic;
 	rescount: out std_logic;
-	middelstemuisknop: out std_logic;
 	handshakeimc: out std_logic);
 end component;
-signal handshakeimx, handshakeimy,handshakeimc:std_logic;
-signal sig_x, sig_y : std_logic_vector(3 downto 0);
+signal s_handshakeimx, s_handshakeimy, s_handshakeimc: std_logic;
+
 begin
-handshakeim <= (handshakeimx and handshakeimy and handshakeimc);
-x1: x port map (clk=>clk,reset=>reset,dx=>dx,handshakemi=>handshakemi,tempx=>sig_x,handshakeimx=>handshakeimx);  
-y1: y port map (clk=>clk,reset=>reset,dy=>dy,handshakemi=>handshakemi,tempy=>sig_y,handshakeimy=>handshakeimy);  
-color1: color port map (clk=>clk,reset=>reset,buttons=>buttons,handshakemi=>handshakemi,countlow=>countlow,output_color=>output_color,draw=>draw,rescount=>rescount,middelstemuisknop => middelstemuisknop,handshakeimc=>handshakeimc); 
-process (clk) begin
-	tempx <= sig_x;
-	tempy <= sig_y;
+
+process(clk)
+begin
+	if (clk'event and clk='1') then
+		handshakeim<=( s_handshakeimx and s_handshakeimy and s_handshakeimc);
+	end if;
 end process;
-end behaviour_logic_top;
+
+x1: x port map (clk=>clk,reset=>reset,dx=>dx,handshakemi=>handshakemi,tempx=>tempx,handshakeimx=>s_handshakeimx);  
+y1: y port map (clk=>clk,reset=>reset,dy=>dy,handshakemi=>handshakemi,tempy=>tempy,handshakeimy=>s_handshakeimy);  
+color1: color port map (clk=>clk,reset=>reset,buttons=>buttons,handshakemi=>handshakemi,countlow=>countlow,output_color=>output_color,draw=>draw,rescount=>rescount,handshakeimc=>s_handshakeimc);  
+end behaviour_top;
+
