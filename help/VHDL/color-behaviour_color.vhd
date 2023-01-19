2ca1b7d7b0d0ac6e8c4cb32c4c17bff36ca95ea8
naufalelkhatibi <106252099+naufalelkhatibi@users.noreply.github.com>
2023-01-19 16:43:10 +0100

    Update color-behaviour_color.vhd

--- help/VHDL/color-behaviour_color.vhd
+++ help/VHDL/color-behaviour_color.vhd
@@ -9,8 +9,9 @@ signal state_hs,next_state_hs: state_type_hs;
 signal sel: std_logic;
 
 begin
+
 reg2: process(clk)
-begin
+    begin 
     if (clk'event and clk='1') then
         if (reset='1') then
             state_hs<=idle;
@@ -21,14 +22,14 @@ begin
 end process;
 
 Handshake: process(handshakemi, state)
-    begin
-    case state_hs is
-        when idle =>
+    begin 
+    case state_hs is 
+        when idle => 
             sel<='0';
             handshakeimc<='0';
-            if (handshakemi='1') then
+            if (handshakemi='1') then 
                 next_state_hs<=increment;
-            else
+            else 
                 next_state_hs<= idle;
             end if;
 
@@ -40,23 +41,25 @@ Handshake: process(handshakemi, state)
         when ready =>
             sel<='0';
             handshakeimc<= '1';
-            if (handshakemi='0') then
+            if (handshakemi='0') then 
                 next_state_hs<= idle;
-            else
+            else 
                 next_state_hs<= ready;
             end if;
         end case;
     end process;
-		
+
+
+
 reg: process(clk)
 	begin
 	if (clk'event and clk='1') then
 		if (reset='1') then
-			state <= cyaan;
+			state <= zwart;
 		else
 			state <= next_state;
 		end if;
-	else
+	
 	end if;
 end process;
 
@@ -64,65 +67,84 @@ end process;
 
 process(buttons,state,handshakemi)
 begin
+
 	case state is 
 	when zwart=>
 	output_color<="000";
-	if (buttons(0)='1' and sel='1') then
+	if (buttons(0)='1' and sel= '1') then
 		next_state<= blauw;
+		
 	else
 		next_state<=zwart;
+		
 	end if;
 	when blauw=>
-	output_color<="001";
-	if (buttons(0)='1' and sel='1') then
+	output_color<="100";
+	if (buttons(0)='1' and sel= '1') then
 		next_state<= groen;
+		
 	else
 		next_state<=blauw;
+		
 	end if;
 	when groen=>
 	output_color<="010";
-	if (buttons(0)='1' and sel='1') then
+	if (buttons(0)='1' and sel= '1') then
 		next_state<= cyaan;
+		
 	else
 		next_state<=groen;
+		
 	end if;
 	when cyaan=>
-	output_color<="011";
-	if (buttons(0)='1' and sel='1') then
+	output_color<="110";
+	if (buttons(0)='1' and sel= '1') then
 		next_state<= rood;
+		
 	else
 		next_state<=cyaan;
+		
 	end if;
 	when rood=>
-	output_color<="100";
-	if (buttons(0)='1' and sel='1') then
+	output_color<="001";
+	if (buttons(0)='1' and sel= '1') then
 		next_state<= paars;
+		
 	else
 		next_state<=rood;
+		
 	end if;
 	when paars=>
 	output_color<="101";
-	if (buttons(0)='1' and sel='1') then
+	if (buttons(0)='1' and sel= '1') then
 		next_state<= geel;
+		
 	else
 		next_state<=paars;
+		
 	end if;
 	when geel=>
-	output_color<="110";
-	if (buttons(0)='1' and sel='1') then
+	output_color<="011";
+	if (buttons(0)='1' and sel= '1') then
 		next_state<= wit;
+		
 	else
 		next_state<=geel;
+		
 	end if;
 	when wit=>
 	output_color<="111";
-	if (buttons(0)='1' and sel='1') then
+	if (buttons(0)='1' and sel= '1') then
 		next_state<= zwart;
+		
 	else
 		next_state<=wit;
+		
 	end if;
 		
 	end case;
+ 
+ 
 
 
 end process;
@@ -142,9 +164,9 @@ begin
 		else
 			rescount<='0';
 		end if;
-	else
+	
 	end if;
-middelstemuisknop<=buttons(1);
 end process;
 
 end behaviour_color;
+
