65f4d1fcf28d07a1961090be6436672b34951239
Joeyinlin <118261782+Joeyinlin@users.noreply.github.com>
2023-01-20 14:43:06 +0100

    Update color.vhd

--- help/VHDL/color.vhd
+++ help/VHDL/color.vhd
@@ -10,7 +10,7 @@ entity color is
 	output_color: out std_logic_vector(2 downto 0);
 	draw: out std_logic;
 	rescount: out std_logic;
-	middelstemuisknop: out std_logic;
+	middelsteknop: out std_logic;
 	handshakeimc: out std_logic);
 end color;
 
